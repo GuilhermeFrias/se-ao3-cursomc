4e636453214899404544e254b752ccaf6736d660
GuilhermeFrias <guilhermesfrias@hotmail.com>
2021-04-13 14:48:14 -0300

    creat instantiation

--- src/main/java/com/guilhermefrias/cursomc/CrusomcApplication.java
+++ src/main/java/com/guilhermefrias/cursomc/CrusomcApplication.java
@@ -1,13 +1,33 @@
 package com.guilhermefrias.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CrusomcApplication {
+import com.guilhermefrias.cursomc.domain.Categoria;
+import com.guilhermefrias.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CrusomcApplication implements CommandLineRunner
+{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrusomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
