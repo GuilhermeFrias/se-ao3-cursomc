a90d35e21f5c0da3ba0100c54f07ab07559bc227
guilherme <guilhermesfrias@hotmail.com>
2021-04-15 16:02:35 -0300

    order, paymentStatus and payment

--- src/main/java/com/guilhermefrias/cursomc/CrusomcApplication.java
+++ src/main/java/com/guilhermefrias/cursomc/CrusomcApplication.java
@@ -1,5 +1,6 @@
 package com.guilhermefrias.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.guilhermefrias.cursomc.domain.Cidade;
 import com.guilhermefrias.cursomc.domain.Cliente;
 import com.guilhermefrias.cursomc.domain.Endereco;
 import com.guilhermefrias.cursomc.domain.Estado;
+import com.guilhermefrias.cursomc.domain.Pagamento;
+import com.guilhermefrias.cursomc.domain.PagamentoComBoleto;
+import com.guilhermefrias.cursomc.domain.PagamentoComCartao;
+import com.guilhermefrias.cursomc.domain.Pedido;
 import com.guilhermefrias.cursomc.domain.Produto;
+import com.guilhermefrias.cursomc.domain.enums.EstadoPagamento;
 import com.guilhermefrias.cursomc.domain.enums.TipoCliente;
 import com.guilhermefrias.cursomc.repositories.CategoriaRepository;
 import com.guilhermefrias.cursomc.repositories.CidadeRepository;
 import com.guilhermefrias.cursomc.repositories.ClienteRepository;
 import com.guilhermefrias.cursomc.repositories.EnderecoRepository;
 import com.guilhermefrias.cursomc.repositories.EstadoRepository;
+import com.guilhermefrias.cursomc.repositories.PagamentoRepository;
+import com.guilhermefrias.cursomc.repositories.PedidoRepository;
 import com.guilhermefrias.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CrusomcApplication implements CommandLineRunner
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired 
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CrusomcApplication.class, args);
@@ -83,6 +95,21 @@ public class CrusomcApplication implements CommandLineRunner
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd//MM//yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("15/04/2021 10:31"),cli1, e1) ;
+		Pedido ped2 = new Pedido(null, sdf.parse("16/04/2021 18:30"),cli1, e2 );
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/04/2021 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 		
 		
---
+++ src/main/java/com/guilhermefrias/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.guilhermefrias.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guilhermefrias.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
