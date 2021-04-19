35cef6125733e5aae9920539cf57bfdcf71f77aa
guilherme <guilhermesfrias@hotmail.com>
2021-04-19 13:29:30 -0300

    creat pages with optional paramters

--- src/main/java/com/guilhermefrias/cursomc/services/CategoriaService.java
+++ src/main/java/com/guilhermefrias/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.guilhermefrias.cursomc.services;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.guilhermefrias.cursomc.domain.Categoria;
@@ -44,6 +46,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	
